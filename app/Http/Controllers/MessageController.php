80372ad395af322dce42755d916008546d0d2254
‘Ibra‘ <ibracortes@gmail.com>
2022-08-23 16:57:28 +0200

    feat:Ver mensajes usando orderby

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,99 +8,97 @@ use Illuminate\Support\Facades\Log;
 
 class MessageController extends Controller
 {
-    public function createMessage(Request $request, $id){
+    public function createMessage(Request $request, $id)
+    {
         try {
-            
+
             $text = $request->input('text');
-            $canalId =$id;
-            $userId = auth()->user()->id;         
+            $canalId = $id;
+            $userId = auth()->user()->id;
             $message = new Message();
             $message->text = $text;
             $message->canal_id = $canalId;
             $message->user_id = $userId;
-            $message->save(); 
-           
+            $message->save();
+
             return response()->json(
                 [
-                    'success'=> true,
-                    'message'=> 'mensaje creado con exito',
-                    'data'=> $text
+                    'success' => true,
+                    'message' => 'mensaje creado con exito',
+                    'data' => $text
                 ],
-            200
+                200
             );
-
-        }catch (\Exception $exception){
+        } catch (\Exception $exception) {
             Log::error('Error no puedes crear un mensaje' . $exception->getMessage());
-   
-               return response()->json(
-                   [
-                       'success' => false,
-                       'message' => 'No puedes crear un mensaje',
-                   ], 
-               400
-               );
-           }
+
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'No puedes crear un mensaje',
+                ],
+                400
+            );
+        }
     }
 
-    public function seeMessage ($id){
+    public function seeMessage($id)
+    {
         try {
             $canalId = $id;
-            $messages = Message::query()->where('canal_id', $canalId)->get('text', 'created_at');
-            $sortMessages = $messages->sortByDesc('created_at');
-            $sortMessages->values()->all();
+            $messages = Message::query()->where('canal_id', $canalId)->orderBy('id', 'ASC')->get(['text', 'created_at']);
 
-
-                     return response()->json(
+            return response()->json(
                 [
                     'success' => true,
                     'message' => 'Aqui puedes ver los mensajes',
                     'data' => $messages
-                ], 
-              200
+                ],
+                200
             );
-
-        }catch (\Exception $exception){
+        } catch (\Exception $exception) {
             Log::error('Error no puedes ver los mensajes' . $exception->getMessage());
-   
-               return response()->json(
-                   [
-                       'success' => false,
-                       'message' => 'No puedes ver los mensajes',
-                   ], 
-               400
-               );
-           }
+
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'No puedes ver los mensajes',
+                ],
+                400
+            );
+        }
     }
-    public function deleteMessage ($id){
+    public function deleteMessage($id)
+    {
         try {
             $messages = Message::query()->where('id', $id);
-            $messages->delete();  
-                     return response()->json(
+            $messages->delete();
+            return response()->json(
                 [
                     'success' => true,
                     'message' => 'mensaje eliminado',
                     'data' => $messages
-                ], 
-              200
+                ],
+                200
             );
-
-        }catch (\Exception $exception){
+        } catch (\Exception $exception) {
             Log::error('Error no puedes eliminar el mensaje' . $exception->getMessage());
-   
-               return response()->json(
-                   [
-                       'success' => false,
-                       'message' => 'No puedes eliminar el mensaje',
-                   ], 
-               400
-               );
-           }
+
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'No puedes eliminar el mensaje',
+                ],
+                400
+            );
+        }
     }
-    public function updateMessage(Request $request, $id){
+    public function updateMessage(Request $request, $id)
+    {
         try {
             $userId = auth()->user()->id;
             $text  = Message::query()->where('user_id', $userId)->find($id);
-            if(!$text){
+            if (!$text) {
                 return response()->json(
                     [
                         'success' => false,
@@ -108,27 +106,26 @@ class MessageController extends Controller
                     ]
                 );
             }
-            $text->text= $request->input('text');
+            $text->text = $request->input('text');
             $text->save();
             return response()->json(
-            [
-                "success" => true,
-                "message" => 'mensaje actualizado',
-                "data" => $text
-            ],
-        200
-        );
-        }catch (\Exception $exception) {
-            Log::error('Error al actualizar este mensaje' .$exception->getMessage());
+                [
+                    "success" => true,
+                    "message" => 'mensaje actualizado',
+                    "data" => $text
+                ],
+                200
+            );
+        } catch (\Exception $exception) {
+            Log::error('Error al actualizar este mensaje' . $exception->getMessage());
             return response()->json(
                 [
                     "success" => false,
                     "message" => 'Error al actualizar mensaje',
                     "error" => $exception->getMessage()
                 ],
-            500
+                500
             );
         }
     }
-
 }
